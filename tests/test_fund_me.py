0896c627c890dccce4f6a5844c7157dcd058c106
gnoparus <surapong@gmail.com>
2022-02-09 00:13:02 +0700

    Finished test_can_fund_and_withdraw.

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -5,5 +5,11 @@ from scripts.deploy import deploy_fund_me
 
 def test_can_fund_and_withdraw():
     account = get_account()
-    deploy_fund_me()
-    fund_me = FundMe[0]
+    fund_me = deploy_fund_me()
+    entrance_fee = fund_me.getEntranceFee()
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == 0
