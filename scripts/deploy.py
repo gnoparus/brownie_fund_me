51eaf3f60d228827f17c0dec39b0c6963ae4344c
gnoparus <surapong@gmail.com>
2022-02-08 12:11:40 +0700

    Externalized publish_source.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -9,14 +9,18 @@ def deploy_fund_me():
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
+        publish_source = True
     else:
         print(f"The active network is {network.show_active()}")
         print(f"Deploying Mocks")
         mock_aggregator = MockV3Aggregator.deploy(18, 2000 * 10**8, {"from": account})
         print(f"Mocks deployed.")
         price_feed_address = mock_aggregator.address
+        publish_source = False
 
-    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=True)
+    fund_me = FundMe.deploy(
+        price_feed_address, {"from": account}, publish_source=publish_source
+    )
     # fund_me = FundMe.deploy({"from": account})
     print(f"contract deployed to {fund_me.address}")
 
