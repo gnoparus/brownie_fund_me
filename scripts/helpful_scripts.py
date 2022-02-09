bffd718ac27d61e37d775c3104a51d9c56ac666c
gnoparus <surapong@gmail.com>
2022-02-09 22:44:26 +0700

    Added mainnet-fork-dev network.

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,11 +3,16 @@ from web3 import Web3
 
 DECIMALS = 8
 STARTING_PRICE = 2000 * 10**10
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
